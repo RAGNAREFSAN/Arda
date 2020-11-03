0f1f0a31e1c4a1e1acf4c7c3516b9993976926f4
Glitch (hello-express) <none>
2020-11-03 07:07:54 +0000

    🦀📒 Checkpoint ./komutlar/kayıt-erkek.js:21235391/1349

--- komutlar/kayıt-erkek.js
+++ komutlar/kayıt-erkek.js
@@ -24,14 +24,20 @@ stg.setNickname(`${tag} ${isim} | ${yas}`)
 stg.roles.add(kayıtlı)
 stg.roles.remove(kayıtsız)
 
+db.add(`kayıtSayi.${message.author.id}`, 1)
+db.add(`erkekUye.${message.author.id}`, 1)
+let erkek = db.get(`erkekUye.${message.author.id}`);
+let kayıtlar = db.fetch(`kayıtSayi.${message.author.id}`); 
 const embed = new Discord.MessageEmbed()
-.setTitle(`Kayıt İşlemi Tamamlandı`)
-    .addField(`Kayıt Eden`, `<@${message.author.id}>`) 
-    .addField(`Verilen Rol`, `<@&${kayıtlı.id}>`) 
-    .addField(`Alınan Rol`, `<@&${kayıtsız.id}>`)
-    .addField(`Yeni İsmin`, `\`${tag} | ${isim} | ${yas}\``) 
+.setTitle(`<a:kelebek:773077162948296775> Kayıt İşlemi Tamamlandı`)
+    .addField(`<a:yildiz:773077160733704203> Kayıt Eden:`, `<@${message.author.id}> Tarafından Kayıt Edildi`) 
+    .addField(`<a:prizma:773077138965266443> Kayıt Edilen:`, `<@${stg.user.id}> Kayıt Oldu`)
+    .addField(`<a:space:773077533495001108> Verilen Rol:`, `<@&${kayıtlı.id}> Rolleri Verildi`) 
+    .addField(`<a:hayir:773077135883632660> Alınan Rol:`, `<@&${kayıtsız.id}> Rolleri Alındı`)
+    .addField(`<a:bitaqua:773077155125919754> Yeni İsmin:`, `\`${tag} | ${isim} | ${yas}\` Olarak Güncellendi`) 
+    .addField(`<a:alev:773077129259909171> Yetkili Toplam:`, `\`${kayıtlar}\` Kayıtlara Sahip.`)
 .setFooter(`Striga #Code`)
-.setColor('0x964396')
+.setColor('GREEN')
 client.channels.cache.get('772228722572001300').send(embed)
   
 }
