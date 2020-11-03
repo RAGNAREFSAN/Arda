99959f34fc63777cfe90478d7215f3896f01d65e
Glitch (hello-express) <none>
2020-11-03 08:04:24 +0000

    ⚓️🐘 Checkpoint ./events/message.js:21235391/2842 ./util/eventLoader.js:21235391/3854 ./package.json:21235391/3358

--- events/message.js
+++ events/message.js
@@ -1,15 +1,70 @@
-const prefix = process.env.PREFIX;
+const Discord = require("discord.js");
+const ayarlar = require('../ayarlar.json');
+let talkedRecently = new Set();
 
-module.exports = (client, message) => {
+module.exports = message => {
+  if (talkedRecently.has(message.author.id)) {
+    return;
+  }
+  talkedRecently.add(message.author.id);
+	setTimeout(() => {
+    talkedRecently.delete(message.author.id);
+  }, 2500);
+  let client = message.client;
   if (message.author.bot) return;
-  if (message.channel.type == "dm") return;
+  if (!message.content.startsWith(ayarlar.prefix)) return;
+  let command = message.content.split(' ')[0].slice(ayarlar.prefix.length);
+  let params = message.content.split(' ').slice(1);
+  let perms = client.elevation(message);
+  let cmd;
+  if (client.commands.has(command)) {
+    cmd = client.commands.get(command);
+  } else if (client.aliases.has(command)) {
+    cmd = client.commands.get(client.aliases.get(command));
+  }
+  if (cmd) {
+   if (cmd.conf.enabled === false) {
+      if (!ayarlar.sahip.includes(message.author.id) && !ayarlar.sahip.includes(message.author.id)) {
+        const embed = new Discord.RichEmbed()
+                    .setDescription(`:x: **${cmd.help.name}** isimli komut şuanda geçici olarak kullanıma kapalıdır!`)
+                    .setColor("RED")
+                message.channel.send({embed})
+                return
+      }
+    }
+    
+    if (cmd.conf.permLevel === 1) {
+			if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+				message.reply(`Bu komutu kullanabilmek için \`Mesajları Yönet\` iznine sahip olmalısın. ❌`)
+				return
+			}
+		}
+		if (cmd.conf.permLevel === 2) {
+			if (!message.member.hasPermission("KICK_MEMBERS")) {
+				message.reply(`Bu komutu kullanabilmek için \`Üyeleri At\` iznine sahip olmalısın. ❌`)
+				return
+			}
+		}
+    if (cmd.conf.permLevel === 3) {
+			if (!message.member.hasPermission("BAN_MEMBERS")) {
+				message.reply(`Bu komutu kullanabilmek için \`Üyeleri Yasakla\` iznine sahip olmalısın. ❌`)
+				return
+			}
+		}
+		if (cmd.conf.permLevel === 4) {
+			if (!message.member.hasPermission("ADMINISTRATOR")) {
+					message.reply(`Bu komutu kullanabilmek için \`Yönetici\` iznine sahip olmalısın. ❌`)
+				return
+			}
+		}
+		if (cmd.conf.permLevel === 5) {
+			if (!ayarlar.sahip.includes(message.author.id)) {
+					message.reply(`Bu komutu sadece \`Sahibim\` kullanabilir. ❌`)
+				return
+			}
+		}
+    if (perms < cmd.conf.permLevel) return;
+    cmd.run(client, message, params, perms);
+  }
 
-  const msgArr = message.content.split(/\s+/g);
-  const command = msgArr[0];
-  const args = msgArr.slice(1);
-
-  if (!command.startsWith(prefix)) return;
-
-  let cmd = client.commands.get(command.slice(prefix.length)) || client.commands.get(client.aliases.get(command.slice(prefix.length)));
-  if (cmd) cmd.run(client, message, args);
 };
