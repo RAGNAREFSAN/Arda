296cd12fc13b0fc2e8a485c0c96d109d61fb23ba
Glitch (hello-express) <none>
2020-11-03 07:30:25 +0000

    👫🛍 Checkpoint ./komutlar/kayıt-sorgu.js:21235391/8646

--- komutlar/kayıt-sorgu.js
+++ komutlar/kayıt-sorgu.js
@@ -3,7 +3,7 @@ const db = require('quick.db')
 
 exports.run = async (client, message, args) => {
 
-    if(!message.member.roles.cache.some(r => ["746467799739793450", "753395862352167032"].includes(r.id)) && (!message.member.hasPermission("ADMINISTRATOR")))
+    if(!message.member.roles.cache.some(r => ["771457523281559552", "771695296291668000"].includes(r.id)) && (!message.member.hasPermission("ADMINISTRATOR")))
     return message.reply("Bu Komutu Kullanmak İçin Yetkiniz Bulunmamakta.")
  
  let kullanıcı = message.mentions.users.first()
@@ -15,18 +15,53 @@ let erkek = db.fetch(`erkekUye.${message.author.id}`);
 let kadın = db.fetch(`kadinUye.${message.author.id}`);
 let kayıtlar = db.fetch(`kayıtSayi.${message.author.id}`); 
 if(erkek === null) erkek = "0"  
-if(erkek === undefined) 
-if(kadın === null)
-if(kadın === undefined)
-if(kayıtlar === null)  
-if(kayıtlar === undefined)
+if(erkek === undefined) erkek = "0"
+if(kadın === null) kadın = "0"
+if(kadın === undefined) kadın = "0"
+if(kayıtlar === null) kayıtlar = "0"
+if(kayıtlar === undefined) kayıtlar = "0"
+  
+const sorgu1 = new Discord.MessageEmbed()
+.setThumbnail(message.author.avatarURL ({ dynamic: true}))
+.setAuthor(message.author.username, message.author.avatarURL)
+.setDescription(`★ Toplam Kayıtların: \`${kayıtlar}\`
+✾ Toplam Erkek Kayıtların: \`${erkek}\`
+❀ Toplam Kadın Kayıtların: \`${kadın}\``)
+.setFooter(`Sadxmamy ❤️ Striga`) // evet bilerek kaldırmadım ayriyetten sıfırdan yazdım :)
+ return message.channel.send(sorgu1)
+};
+  
+if(kullanıcı) {  
+let erkek1 = db.fetch(`erkekUye.${kullanıcı.id}`);
+let kadın1 = db.fetch(`kadinUye.${kullanıcı.id}`);
+let kayıtlar1 = db.fetch(`kayıtSayi.${kullanıcı.id}`); 
+if(erkek1 === null) erkek1 = "0"  
+if(erkek1 === undefined) erkek1 = "0"
+if(kadın1 === null) kadın1 = "0"
+if(kadın1 === undefined) kadın1 = "0"
+if(kayıtlar1 === null) kayıtlar1 = "0"
+if(kayıtlar1 === undefined) kayıtlar1 = "0"
+  
+const sorgu2 = new Discord.MessageEmbed()
+.setThumbnail(kullanıcı.avatarURL ({ dynamic: true})) 
+.setAuthor(`${kullanıcı.username}`)
+.setDescription(`★ Toplam Kayıtların: \`${kayıtlar1}\`
+✾ Toplam Erkek Kayıtların: \`${erkek1}\`
+❀ Toplam Kadın Kayıtların: \`${kadın1}\``)
+.setFooter(`Sadxmamy ❤️ Striga`) // evet bilerek kaldırmadım ayriyetten sıfırdan yazdım :)
+ return message.channel.send(sorgu2)
   
 };
+  
+  };
 
 exports.conf = {
     enabled: true,
     guildOnly: true,
     aliases: ["stat", "kayıtlar", "kayıt-kontrol"],
     permLvl: 0,
-    name: "stat"
+}
+  
+exports.help = {  
+  name: "stat"
 }
