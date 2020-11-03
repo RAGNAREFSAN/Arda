06ff425ec073617c93b2b56a3d515c69f3b75f27
Glitch (hello-express) <none>
2020-11-03 08:15:51 +0000

    🗽🏖 Checkpoint ./util/eventLoader.js:21235391/18502 ./bot.js:21235391/80 ./events/message.js:21235391/16706 ./events/guildMemberAdd.js:21235391/26

--- events/message.js
+++ events/message.js
@@ -9,7 +9,7 @@ module.exports = message => {
   talkedRecently.add(message.author.id);
 	setTimeout(() => {
     talkedRecently.delete(message.author.id);
-  }, 2500);
+  }, 0);
   let client = message.client;
   if (message.author.bot) return;
   if (!message.content.startsWith(ayarlar.prefix)) return;
@@ -23,46 +23,6 @@ module.exports = message => {
     cmd = client.commands.get(client.aliases.get(command));
   }
   if (cmd) {
-   if (cmd.conf.enabled === false) {
-      if (!ayarlar.sahip.includes(message.author.id) && !ayarlar.sahip.includes(message.author.id)) {
-        const embed = new Discord.RichEmbed()
-                    .setDescription(`:x: **${cmd.help.name}** isimli komut şuanda geçici olarak kullanıma kapalıdır!`)
-                    .setColor("RED")
-                message.channel.send({embed})
-                return
-      }
-    }
-    
-    if (cmd.conf.permLevel === 1) {
-			if (!message.member.hasPermission("MANAGE_MESSAGES")) {
-				message.reply(`Bu komutu kullanabilmek için \`Mesajları Yönet\` iznine sahip olmalısın. ❌`)
-				return
-			}
-		}
-		if (cmd.conf.permLevel === 2) {
-			if (!message.member.hasPermission("KICK_MEMBERS")) {
-				message.reply(`Bu komutu kullanabilmek için \`Üyeleri At\` iznine sahip olmalısın. ❌`)
-				return
-			}
-		}
-    if (cmd.conf.permLevel === 3) {
-			if (!message.member.hasPermission("BAN_MEMBERS")) {
-				message.reply(`Bu komutu kullanabilmek için \`Üyeleri Yasakla\` iznine sahip olmalısın. ❌`)
-				return
-			}
-		}
-		if (cmd.conf.permLevel === 4) {
-			if (!message.member.hasPermission("ADMINISTRATOR")) {
-					message.reply(`Bu komutu kullanabilmek için \`Yönetici\` iznine sahip olmalısın. ❌`)
-				return
-			}
-		}
-		if (cmd.conf.permLevel === 5) {
-			if (!ayarlar.sahip.includes(message.author.id)) {
-					message.reply(`Bu komutu sadece \`Sahibim\` kullanabilir. ❌`)
-				return
-			}
-		}
     if (perms < cmd.conf.permLevel) return;
     cmd.run(client, message, params, perms);
   }
