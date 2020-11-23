c7e3707a6b7ec38cc2cfe7e6623687a155609193
Glitch (hello-express) <none>
2020-11-23 11:17:15 +0000

    🐷🏐 Checkpoint ./ayarlar.json:21006217/289 ./komutlar/jail.js:21006217/1376 ./events/ready.js:21006217/347

--- events/ready.js
+++ events/ready.js
@@ -10,6 +10,6 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("dnd");
   client.user.setActivity("STGXMAMY", { type: "WATCHING"}); //// TYPE - WATCHING , PLAYING , LISTENING gibi değiştirilebilir.
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+  console.log(``);
+
 };
