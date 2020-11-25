2f39c7e15e8547f0d02f0fa89fd36e6459048ce4
Glitch (hello-express) <none>
2020-11-25 15:24:26 +0000

    🏵👰 Checkpoint ./komutlar/kayıt-topteyit.js:15761012/146:21006217/231

--- komutlar/kayıt-topteyit.js
+++ komutlar/kayıt-topteyit.js
@@ -5,10 +5,10 @@ exports.run = async (client, message, member) => {
   
 let uye = message.mentions.users.first() || message.author;
 let bilgi = db.get(`yetkili.${uye.id}.toplam`);
-
+let yazı = "Top Özellik Listesi"
   
-let top = message.guild.members.cache.filter(uye => db.get(`yetkili.${uye}.toplam`)).array().sort((uye1, uye2) => Number(db.get(`yetkili.${uye2}.toplam`))-Number(db.get(`yetkili.${uye1}.toplam`))).slice(0, 20).map((uye, index) => (index+1)+"-) "+ uye + " | " + db.get(`yetkili.${uye.id}.toplam`)).join('\n');
-message.channel.send(new dc.MessageEmbed().setTitle('Top Özellik Listesi').setTimestamp().setFooter(message.member.displayName+" tarafından istendi!", message.author.avatarURL).setDescription(top).setColor("RANDOM"));
+let top = message.guild.members.cache.filter(uye => db.get(`yetkili.${uye.id}.toplam`)).array().sort((uye1, uye2) => Number(db.get(`yetkili.${uye2.id}.toplam`))-Number(db.get(`yetkili.${uye1.id}.toplam`))).slice(0, 15).map((uye, index) => (index+1)+" • <@"+ uye +"> | " + db.get(`yetkili.${uye.id}.toplam`)).join('\n');
+message.channel.send(new dc.MessageEmbed().setAuthor(yazı, message.guild.iconURL({dynamic: true})).setTimestamp().setFooter(message.member.displayName+" tarafından istendi!", message.author.avatarURL).setDescription(top).setColor("RANDOM"));
   
 }
 
@@ -22,4 +22,3 @@ exports.conf = {
 exports.help = {
     name: "topteyit"
 }
-
